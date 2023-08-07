20c8ecb6f1018b03451587ec54864e916ad04156
Kirill Müller <kirill@cynkra.com>
2023-08-07 18:32:26 +0200

    f

--- tools/rpkg/src/types.cpp
+++ tools/rpkg/src/types.cpp
@@ -249,6 +249,9 @@ LogicalType RApiTypes::LogicalTypeFromRType(const RType &rtype, bool experimenta
 			children.push_back(
 			    std::make_pair(child.first, RApiTypes::LogicalTypeFromRType(child.second, experimental)));
 		}
+		if (children.size() == 0) {
+			cpp11::stop("rapi_execute: Packed column must have at least one column");
+		}
 		return LogicalType::STRUCT(std::move(children));
 	}
 
